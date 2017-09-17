d7719626c8c2f3f7a20e153494692bdb76d60e8c
Carlos Abraham <abraham@abraham.ml>
2017-09-17 03:46:44 -0400

    Create hello.c

---
+++ pset2/hello.c
@@ -0,0 +1,19 @@
+/*
+*
+*  	Write a to print Hello, world!
+*   
+*   Author: Carlos Abraham Hernandez, www.19cah.ml
+*	
+*  	 twitter.com/19cah    github.com/19cah
+*  	 facebook.com/19cah	 stackoverflow.com/story/19cah
+*	 instagram.com/19cah	 linkedin.com/in/19cah
+*	 bitbucket.org/19cah   repl.it/@19cah
+*
+*/
+ 
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+}
